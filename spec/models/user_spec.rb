71769caeb07c89a354a87f605f85c725f79dc0a4
Phreemason <github email address>
2017-06-25 19:13:00 -0400

    passing user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,27 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it 'requires an email and password and username upon creation' do
-      user=create(:user, username: nil, password: nil, email: nil)
+      user=build(:user, username: nil, password: nil, email: nil)
 
       expect(user.valid?).to equal(false) 
-      expect(user.errors.full_messages).to eq(
-        "Password can't be blank, Email can't be blank"
-      )
+      expect(user.errors.full_messages).to eq([
+      	"Password can't be blank", 
+      	"Email can't be blank"	
+      ])
 
     end
+
+    it 'requires a valid email' do
+      create(:user) 
+      user = build(:user)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([ 
+      	"Email has already been taken"	
+      ])
+
+    end
+
 
     it 'requires a unique email'
 
