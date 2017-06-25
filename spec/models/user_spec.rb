6e076a167aeac153b4432f1f097ae431c584c6fb
Phreemason <github email address>
2017-06-25 19:04:39 -0400

    created first test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'validations' do
-    it 'requires an email and password and username upon creation'
+    it 'requires an email and password and username upon creation' do
+      user=create(:user, username: nil, password: nil, email: nil)
+
+      expect(user.valid?).to equal(false) 
+      expect(user.errors.full_messages).to eq(
+        "Password can't be blank, Email can't be blank"
+      )
+
+    end
 
     it 'requires a unique email'
 
   end
+
+  describe 'on save' do
+    it 'hashes a password'
+
+  end
+
   
   describe 'relationships' do
   	it 'has many games'
