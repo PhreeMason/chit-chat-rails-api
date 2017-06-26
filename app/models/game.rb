a29d0638067d69525b242d8cbf924247fdc29e11
Phreemason <github email address>
2017-06-26 00:55:29 -0400

    created method for distributing dominos after game has begun

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,13 +2,31 @@ class Game < ApplicationRecord
 	validates :users, length: { is: 4 }
 	has_many :game_players
 	has_many :users, through: :game_players
-
-  @@tiles = ["zero-zero", "zero-one", "zero-two", "zero-three", "zero-four", "zero-five", "zero-six", "one-one", "one-two", "one-three", "one-four", "one-five", "one-six", "two-two", "two-three", "two-four", "two-five", "two-six", "three-three", "three-four", "three-five", "three-six", "four-four", "four-five", "four-six", "five-five", "five-six", "six-six"]
+	after_create :distribute_tiles
+	attr_accessor tiles 
  
   def self.make(users)
     game = Game.new
     users.each{|user| game.users << user}
     game
+  end
+
+  def distribute_tiles
+  	@tiles.shuffle!
+  	self.game_players.each{|player| player.tiles = @tiles.slice(0, 6) }
+  	self.tiles_played = []
+  end
+
+  def shuffle_tiles
+    arr  = ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
+    arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
+    @tiles  = []
+    arr.each do |ele| 
+      arr1.each do |a| 
+        @tiles.push("#{ele}-#{a}") 
+      end
+      arr1.shift
+    end
   end
 
 end
