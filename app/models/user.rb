54adf2b42b1c933c5f320956da1bad74b9ec3ba1
Phreemason <github email address>
2017-06-25 22:48:09 -0400

    created connections between users and games

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true 
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  has_many :game_players
+  has_many :games, through: :game_players  
+  
 end
