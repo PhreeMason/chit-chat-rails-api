d9221f8ff468321c3e81526664384f0f1c97ab98
Phreemason <github email address>
2017-06-25 23:26:34 -0400

    added validator for users on game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,14 @@
 class Game < ApplicationRecord
+	validates :users, length: { is: 4 }
 	has_many :game_players
 	has_many :users, through: :game_players
 
   @@tiles = ["zero-zero", "zero-one", "zero-two", "zero-three", "zero-four", "zero-five", "zero-six", "one-one", "one-two", "one-three", "one-four", "one-five", "one-six", "two-two", "two-three", "two-four", "two-five", "two-six", "three-three", "three-four", "three-five", "three-six", "four-four", "four-five", "four-six", "five-five", "five-six", "six-six"]
  
 
-  def self.make(users: [])
+  def self.make(users)
     game = Game.new
+    users.each{|user| game.users << user}
   end
 
 end
