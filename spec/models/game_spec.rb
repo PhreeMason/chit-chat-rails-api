af5cf4390a74205e8ac2fa3a830ab630b45b893c
Phreemason <github email address>
2017-06-25 22:46:44 -0400

    described inital tests for games

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'initialize' do
+    it 'assigns each their order' do
+
+    end
+
+    it 'gives each player seven dominos' do
+
+    end
+
+    it 'has an empty array of tiles played' do 
+
+    end
+
+    it 'has a status of active' do
+
+    end
+
+  end
+
 end
