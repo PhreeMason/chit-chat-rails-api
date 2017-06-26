311774521ecb7492c0a9310003abfd9a238c19bf
Phreemason <github email address>
2017-06-25 23:54:44 -0400

    passing game tests for initialized users

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:users) {[
+    user1 = create(:user),
+    user2 = create(:user, email: 'email1@email.com', password: 'paswords'), 
+    user3 = create(:user, email: 'email2@email.com', password: 'paswords'),
+    user4 = create(:user, email: 'email3@email.com', password: 'paswords')
+  
+  ]}
 
   describe 'initialize' do
+    it "doesn't save without users" do
+      game1 = Game.new
+      game2 = Game.make(users)
+
+      expect(game1.valid?).to equal(false)
+      expect(game1.errors.full_messages).to eq([
+      	"Users is the wrong length (should be 4 characters)" 
+      ])
+      expect(game2.errors.full_messages).to eq([])
+      expect(game2.valid?).to equal(true)
+    end
+
+
     it 'assigns each their order' do
+
 
     end
 
@@ -12,7 +32,8 @@ RSpec.describe Game, type: :model do
 
     end
 
-    it 'has an empty array of tiles played' do 
+    it 'has an empty array of tiles played' do
+      
 
     end
 
