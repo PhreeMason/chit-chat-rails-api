8e01eb7dd0e0c5bc5b25bf59fb9e7d1d4346c940
Phreemason <github email address>
2017-06-30 03:05:59 -0400

    started writing logic for creating a user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    puts 'something awesome'
+    @user = User.new(user_params)
+    if @user.save
+      #TODO:// input signup logic
+    else
+      render json: { 
+        errors: @user.errors 
+      }, status: 500
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end
