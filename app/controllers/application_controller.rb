c78165ba7e740a9ed500ff12aa79517eba8cedd4
Phreemason <github email address>
2017-06-30 20:40:05 -0400

    created authentication helper method and current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+  helper_method :authenticate_token!, :current_user
+
+  private
+    def authenticate_token!
+      if request.env["HTTP_AUTHORIZATION"]
+        begin
+          token = request.env["HTTP_AUTHORIZATION"].split(" ").last
+          decoded = Auth.decode_token(token)
+          @user_id = decoded[0]["user_id"] if decoded
+        rescue JWT::DecodeError
+          errors = [{ message: 'This token is not valid' }]
+        end
+
+        if !current_user || !decoded || errors
+          render json: { 
+            errors: errors
+          }, status: 403
+        end
+
+      else 
+        render json: { 
+            errors: [ 
+              { message:  "You must include a JWT Token!"  }
+            ]
+          }, status: 403
+      end
+    end 
+
+    def current_user
+      @user ||= User.find_by(id: @user_id) if @user_id
+    end
+    
+    
+
 end
