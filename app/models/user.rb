200665d6f6a1061bf6a46fbc00ecfac7b5659a9a
Phreemason <github email address>
2017-07-07 09:00:56 -0400

    added in new relationships for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   has_many :game_players
-  has_many :games, through: :game_players  
-  
+  has_many :games, through: :game_players 
+  has_many :chatroom_users
+  has_many :chatrooms, through: :chatroom_users 
+  has_many :messages
 end
