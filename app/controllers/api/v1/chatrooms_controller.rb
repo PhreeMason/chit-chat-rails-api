88b8941fc56f6e8cbe069f270793668cd3f01dcd
Phreemason <github email address>
2017-07-11 20:45:48 -0400

    added show route to chatrooms

--- app/controllers/api/v1/chatrooms_controller.rb
+++ app/controllers/api/v1/chatrooms_controller.rb
@@ -3,8 +3,21 @@ class Api::V1::ChatroomsController < ApplicationController
   
   def index
     chatrooms = Chatroom.all
-    render json: chatrooms
+    render json: {chatroom: chatroom.as_json(only: [:id, :name])}
   end
+
+  def show
+    chatroom = Chatroom.find(params[:id])
+    messages = chatroom.messages
+    chatroom_users = chatroom.chatroom_users
+
+    render json: {
+      chatroom: chatroom.as_json(only: [:id, :name]), 
+      message: messages.as_json,
+      users: chatroom_users.as_json(only: [:user_id, :chatroom_id])
+    }
+  end
+
 
 
 end
