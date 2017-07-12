fb57abdc39c203f56b79a8010f220aca8480c59d
Phreemason <github email address>
2017-07-12 17:51:28 -0400

    messages render the username of owner when created

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,10 +1,11 @@
-class MessagesController < ApplicationController
-  
+class Api::V1::MessagesController < ApplicationController
+  before_action :authenticate_token!
+
   def create
     message = Message.new(message_params)
     message.user = current_user
     if message.save
-      render json: message
+      render json: message.as_json(methods: :user_name)
     else 
       render json: { 
         errors: message.errors 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
+
+  def user_name
+    self.user.username
+  end
 end
