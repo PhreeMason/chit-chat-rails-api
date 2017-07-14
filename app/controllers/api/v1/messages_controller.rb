d17be6f5ceb52bb07df81fda177d9c1deb061fdc
Phreemason <github email address>
2017-07-14 07:07:23 -0400

    deleted message controller actions since there are now being completed by the message relay job and chatrooms channel

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,16 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   before_action :authenticate_token!
 
-  def create
-    message = Message.new(message_params)
-    message.user = current_user
-    if message.save
-      render json: message.as_json(methods: :user_name)
-    else 
-      render json: { 
-        errors: message.errors 
-      }, status: 500
-    end
   end
 
   private
