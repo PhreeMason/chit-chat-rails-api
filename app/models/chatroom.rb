2ab0bd1992f83fcbd4c1e21dbb76d170a35efbda
Phreemason <github email address>
2017-07-17 03:22:39 -0400

    addeed private option to chatrooms with validations

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,6 +3,8 @@ class Chatroom < ApplicationRecord
   has_many :users, through: :chatroom_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+  validates :chatroom_users, length: { maximum: 2, too_long: "This is a private chat %{count} is the maximum allowed"}, if: :private
+  validates :name, length: { minimum: 3 }
 
   def user_ids
     self.chatroom_users.map(&:user_id)
