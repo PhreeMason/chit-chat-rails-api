f77966beb4ef14a4d309084b19c5b25bbb109352
Phreemason <github email address>
2017-07-19 16:00:20 -0400

    added option for users to update there profile information

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,35 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_token!, only: :update
+
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: {
-        user: @user.as_json(only: [:id, :username, :email]),
-        token: ::Auth.create_token(@user.id)
-      }
+      render @user
     else
       render json: { 
         errors: @user.errors 
       }, status: 500
     end
   end
-
-  def user_params
-    params.require(:user).permit(:username, :password, :email)
+  
+  def update
+    user = current_user
+    user.update(update_params)
+    render @user
   end
+
+  def show
+    @user = User.find_by(username: params[:username])
+    render @user
+  end
+
+  private
+    
+    def update_params
+      params.require(:user).permit(:bio, :pic_link, :location)
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :password, :email)
+    end
 end
