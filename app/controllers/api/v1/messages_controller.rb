8f8f6e09d1e2fd1e08f185e7afd62e45baef6244
Phreemason <mr.omarmason@gmail.com>
2017-07-22 06:37:04 -0400

    removed message controller as it is not being used

--- app/controllers/api/v1/messages_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::MessagesController < ApplicationController
-  before_action :authenticate_token!
-
-  private
-
-    def message_params
-      params.require(:message).permit(:body, :chatroom_id)
-    end
-end
