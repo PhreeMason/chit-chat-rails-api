cc3504b59f60c93ac9797b0a50b54a122bf2e6dc
Phreemason <github email address>
2017-07-22 03:51:42 -0400

    users can only subscribe to one chatroom at a time

--- app/channels/chatrooms_channel.rb
+++ app/channels/chatrooms_channel.rb
@@ -1,12 +1,11 @@
 class ChatroomsChannel < ApplicationCable::Channel
   
   def subscribed
-    current_user.chatrooms.each do |chatroom|
-      stream_from "chatroom: #{chatroom.id}"
-    end
+    stream_from "chatroom: #{params[:id]}"
   end
   
   def send_message(data)
+    puts data
     @chatroom = Chatroom.find(data["chatroom_id"])
     message = @chatroom.messages.create(body: data["body"], user: current_user) 
     MessageRelayJob.perform_later(message)
